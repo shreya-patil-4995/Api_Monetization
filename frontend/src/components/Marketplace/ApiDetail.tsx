6ff3adf4f633faaaa886a36931371c2d0bae2d91
Shreya Patil <shreyapatil4995@gmail.com>
2026-06-05 18:45:40 +0530

    fix: delete unused submittingReview state to fix ESLint build error

--- frontend/src/components/Marketplace/ApiDetail.tsx
+++ frontend/src/components/Marketplace/ApiDetail.tsx
@@ -70,8 +70,6 @@ const ApiDetail: React.FC = () => {
     const [reviewRating, setReviewRating] = useState(0);
     const [reviewTitle, setReviewTitle] = useState('');
     const [reviewComment, setReviewComment] = useState('');
-    const [_submittingReview, _setSubmittingReview] = useState(false);
-
     const [isSubscribed, setIsSubscribed] = useState(false);
     const [userSubscription, setUserSubscription] = useState<any>(null);
 
